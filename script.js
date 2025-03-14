c38a60bfb0c9b36700c459b68b9526fc0241330f
Nemanja Kosanovic <nemanjakosanovic7@gmail.com>
2025-03-14 16:42:18 +0100

    reviews section, responsive

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ var swiper = new Swiper(".mySwiper", {
     clickable: true,
   },
 });
+var swiper2 = new Swiper(".mySwiper2", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: -111,
+    depth: 500,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
